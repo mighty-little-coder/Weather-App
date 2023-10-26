220b30b82b15dac378d06d17d845248d7568aebf
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-10-25 22:59:30 -0400

    Started JS code

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+var dateIs = dayjs();
+
+var setToday = document.querySelector("todaysDate")
+
+
+
+function today() {
+setToday = dateIs;
+}
+
