1299958c41dad0f3d88febf308bc0f3c9a5f8b93
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-10-28 23:09:50 -0400

    Added HTML to create functioning search button and JS to create functions for logging search field info

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,19 @@
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 var dateIs = dayjs();
-
+var searchDest = document.querySelector("#locationTextBox")
 var setToday = document.querySelector("todaysDate")
-
+var searchSubmit = document.querySelector("#locationSearch")
 
 
 function today() {
 setToday = dateIs;
 }
 
+function setDest(event) {
+  event.preventdefault();
+  searchDest.value = "";
+}
+
+function searchBtn() {
+ searchSubmit.addEventListener("submit", setDest);
+}
