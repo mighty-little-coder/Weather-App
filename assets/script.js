87b850d749674454147ad84d9b10c8a7d4d7d075
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-10-29 15:42:10 -0400

    Added some functionailty to JSON.parse

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,30 @@
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 var dateIs = dayjs();
-var searchDest = document.querySelector("#locationTextBox")
-var setToday = document.querySelector("todaysDate")
-var searchSubmit = document.querySelector("#locationSearch")
+var searchDest = document.querySelector("#locationTextBox");
+var setToday = document.querySelector("todaysDate");
+var searchSubmit = document.querySelector("#locationSearch");
+var pastDest = document.querySelector("#prevListLi");
 
 
 function today() {
-setToday = dateIs;
-}
-
-function setDest(event) {
-  event.preventdefault();
-  searchDest.value = "";
+  setToday.dateIs;
 }
 
 function searchBtn() {
- searchSubmit.addEventListener("submit", setDest);
+  searchSubmit.addEventListener("submit", setDest);
+}
+
+document.querySelector('form').onsubmit = function (e) {
+  e.preventDefault();
+  var dest = searchDest.value;
+  localStorage.setItem("destination", JSON.stringify(dest));
+  console.log(localStorage);
+  searchDest.value = "";
+  var previousDest = JSON.parse(localStorage.getItem("destination"));
+  var ul = document.querySelector("prevList");
+  previousDest.forEach(function(item) {
+    var prevDestEl = document.createElement("listItem");
+    listItem.appendChild(document.createTextNode(item));
+    prevList.appendChild(listItem);
+  });
 }
