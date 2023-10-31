9a1454218a54db48133fb2a0865efd507923f3df
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-10-30 23:56:44 -0400

    Had to reconfigure page and is now VERY lame. Adjusted CSS, HTML, and JAVASCRIPT

--- assets/old-script.js
+++ assets/old-script.js
@@ -1,3 +1,62 @@
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// fetch(queryURL)
+var APIkey = "3ac4c533f75c393e9ad9feff434508cf"
+var searchDest = document.querySelector("#locationTextBox");
+var searchSubmit = document.querySelector("#locationSearch");
+var locations = document.querySelector(".locationEl")
+var setToday = document.querySelector(".todaysDate");
+var pastDest = document.querySelector(".prevList");
+var destinationsArray = []
+var city;
+
+function displayTime() {
+  var currentTime = dayjs().format('dddd MMM DD, YYYY');
+  setToday.textContent = currentTime;
+}
+
+displayTime();
+setInterval(displayTime, 1000);
+
+function storeDest() {
+  var dest = searchDest.value;
+  localStorage.setItem("destination", JSON.stringify(dest));
+  console.log(dest);
+  console.log(localStorage);
+}
+
+function renderDest() {
+  pastDest.innerHTML = "";
+
+  for (var i = 0; i < destinationsArray.length; i++) {
+    var prevDestEl = destinationsArray[i];
+
+    var li = document.createElement("li");
+    li.textContent = pastDest;
+    li.setAttribute("data-index", i);
+
+    var listBtn = document.createElement("button");
+    listBtn.textContent = "Search";
+
+    li.appendChild(listBtn);
+    pastDest.appendChild(li);
+  }
+}
+
+locations.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var dest = searchDest.value.trim();
+
+  destinationsArray.push(dest);
+  searchDest.value = "";
+
+  storeDest();
+  renderDest();
+
+});
+
+
 function generateWeatherInfo(lat, lon) {
   const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherAPIKey}`;
   const forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherAPIKey}`;
@@ -87,13 +146,15 @@ function generateWeatherInfo(lat, lon) {
       console.error(err);
       alert("An error occurred while fetching the forecast data.");
     });
-    if (currentInfoDiv.children.length > 0) {
-      currentInfoDiv.classList.add('active'); // Add the 'active' class to show the current forecast
-    }
+  if (currentInfoDiv.children.length > 0) {
+    currentInfoDiv.classList.add('active'); // Add the 'active' class to show the current forecast
+  }
 }
+
+
 function getCityCoordinates() {
   var cityName = cSearch.value;
-    console.log(cityName + " " + state)
+  console.log(cityName + " " + state)
   if (!cityName) return;
   const geoCodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${weatherAPIKey}`;
   fetch(geoCodingApiUrl)
@@ -116,3 +177,4 @@ function getCityCoordinates() {
       alert("An error occurred while fetching the coordinates.");
     });
 }
+
